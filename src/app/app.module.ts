2b43aa28f37cd57afecc074555120e7df68a5524
Enoch Root <enochroot@openmailbox.org>
2018-10-06 11:05:00 -0300

    [Add] Componente Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule, NgbModule
+    BrowserModule, NgbModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
