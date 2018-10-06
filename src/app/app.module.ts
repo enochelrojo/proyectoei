818d8d6235b017c0cae946a58ab43620c133cbd8
Enoch Root <enochroot@openmailbox.org>
2018-10-06 11:22:09 -0300

    [Add] Componente Tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule, NgbModule, AppRoutingModule
