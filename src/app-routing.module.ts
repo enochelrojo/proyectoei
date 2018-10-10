8d91a109626d40d7777fc3fdfdabe0ede305fa6f
Enoch Root <enochroot@openmailbox.org>
2018-10-09 21:19:24 -0300

    [Mod] Modificaciones varias: routes - favicon - styles

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
+import { TasksComponent } from './app/tasks/tasks.component';
+import { CoursesComponent } from './app/courses/courses.component';
+
+const rutas: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'tareas', component: TasksComponent },
+  { path: 'cursos', component: CoursesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(rutas) ],
+  declarations: [],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
